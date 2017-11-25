5530809871499202f5092f1bd4e0763de9567ef9
spoonman <Chenfmmu@163.com>
2017-11-25 18:12:42 +0800

    Test the get route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,10 +3,17 @@ const request = require("supertest")
 
 const {app} = require("./../server");
 const {Todo} = require("./../models/todo");
+const todos = [{
+  text : "firt test todo"
+}, {
+  text : "second test todo"
+}]
 
 beforeEach((done) => {
-    Todo.remove({}).then(() => {done();});
-})
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
+});
 
 describe("POST /todos", () => {
     it("should create a new todo", (done) => {
@@ -22,7 +29,7 @@ describe("POST /todos", () => {
                 return done(err);
             }
 
-            Todo.find().then((todos) => {
+            Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -43,9 +50,22 @@ describe("POST /todos", () => {
             }
 
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch((e) => {done(e)})
         })
     })
 });
+
+describe("GET /todos", () => {
+  it("should get all the data back", (done) => {
+    request(app)
+    .get("/todos")
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.result.length).toBe(2)
+    })
+    .end(done);
+
+    });
+  });
