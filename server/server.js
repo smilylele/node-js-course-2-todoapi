906d0eb8ba6d2f490c0772020f8a82a26587d4f3
spoonman <Chenfmmu@163.com>
2017-11-25 17:42:13 +0800

    Add app.get to find all items in the todo collection

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post("/todos", (req, res) => {
     })
 });
 
+app.get("/todos", (req, res) => {
+    Todo.find().then((result) => {
+        res.send({result});
+    }, (err) => {
+        res.status(400).send(e);
+    })
+});
+
 app.listen(3000, () => {
     console.log("Started on port 3000");
 });
